4338b9cd845f1456895d78735b38f730e4d7300b
Jacky <jacky8595125@gmail.com>
2022-11-09 18:15:23 +0800

    新增 本地帳戶 驗證 no db

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -3,6 +3,7 @@ import { CreateUserDto } from '../user/dto/create-user.dto';
 import { UserService } from '../user/user.service';
 import { Request } from 'express';
 import { AuthGuard } from '@nestjs/passport';
+import { LocalAuthGuard } from './local-auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -18,7 +19,7 @@ export class AuthController {
   /** 登入
    * 會到 auth.service.ts  validateUser function 去驗證
    */
-  @UseGuards(AuthGuard('local'))
+  @UseGuards(AuthGuard('local')) // 驗證標籤
   @Post('/signin')
   signin(@Req() request: Request){
     // return request.user;
@@ -32,4 +33,25 @@ export class AuthController {
      */
     return request.user;
   }
+
+
+/** 登入 by jwt
+   * 會到 auth.service.ts  validateUser function 去驗證
+   */
+ @UseGuards(LocalAuthGuard) // 驗證標籤
+ @Post('/signinByJwt')
+ signinByJwt(@Req() request: Request){
+   // return request.user;
+
+
+   /**
+    {
+   "username": "jacky8595125@gmail.com",
+   "email": "JK"
+}
+    */
+   return request.user;
+ }
+
+
 }
