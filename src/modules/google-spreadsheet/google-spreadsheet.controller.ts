9de2119212fbeefbe6875abb97e960f0345e456b
Jacky <jacky8595125@gmail.com>
2022-11-14 17:27:10 +0800

    新增 googleSheet 範例

--- src/modules/google-spreadsheet/google-spreadsheet.controller.ts
+++ src/modules/google-spreadsheet/google-spreadsheet.controller.ts
@@ -3,14 +3,17 @@ import { GoogleSpreadsheetService } from './google-spreadsheet.service';
 
 @Controller('google-spreadsheet')
 export class GoogleSpreadsheetController {
-
   constructor(
     private readonly googleSpreadsheetService: GoogleSpreadsheetService,
-  ){}
+  ) {}
 
-    @Get('showInfo')
-    async showInfo(){
-      return await this.googleSpreadsheetService.showInfo()
-    }
 
+  /**
+   * https://docs.google.com/spreadsheets/d/1lV7jAuB_Es3o9ZNDwgH9vyYmsctEsDH5qjjL4Wi0ZCQ/edit#gid=0
+   * @returns
+   */
+  @Get('showInfo')
+  async showInfo() {
+    return await this.googleSpreadsheetService.showInfo();
+  }
 }
