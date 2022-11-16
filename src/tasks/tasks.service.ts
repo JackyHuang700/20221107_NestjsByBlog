35d4843e80f774b2819ce3585bf034709b8decb6
Jacky <jacky8595125@gmail.com>
2022-11-16 13:56:12 +0800

    railway.app

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -7,14 +7,14 @@ export class TasksService {
   private readonly logger = new Logger(TasksService.name);
 
   /** 每分鐘的第45秒執行 */
-  @Cron('30 * * * * *')
+  @Cron('* 10 * * * *')
   // @Cron(CronExpression.EVERY_30_SECONDS)
   handleCron() {
     this.logger.debug('Called when the second is 30');
   }
 
 
-  @Interval(10000)
+  @Interval(1000 * 1000)
   handleInterval() {
     this.logger.debug('Called every 10 seconds');
   }
