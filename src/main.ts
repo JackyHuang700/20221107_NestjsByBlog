ff655cdd20357d8fb3857b4b450dd1edf01ac79c
Jacky <jacky8595125@gmail.com>
2022-11-16 12:07:52 +0800

    add process.env.PORT

--- src/main.ts
+++ src/main.ts
@@ -23,7 +23,7 @@ async function bootstrap() {
   app.enableCors()
   /** end.CORS */
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
